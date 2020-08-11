c7cc0a3c461beb3380186b01f007283a09a00891
Tran <tranc27@medtronic.com>
2020-08-11 11:07:30 -0500

    add primary key for salary table

--- EmployeeSQL/employee_db.sql
+++ EmployeeSQL/employee_db.sql
@@ -21,7 +21,8 @@ CREATE TABLE "employees" (
 CREATE TABLE "salaries" (
     "emp_no" int NOT NULL,
     "salary" int   NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE "departments" (
